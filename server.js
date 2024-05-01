756ff75543fb3b189c12ac65801a177efb7fc1e3
Arya Wibowo Jati <94813894+jateez@users.noreply.github.com>
2024-05-01 14:41:44 +0700

    update materials

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const http = require('http');
  
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('X-Powered-By', 'NodeJS')
     const { url, method } = request;
     if (url === "/") {
         if(method === 'GET') {
             response.statusCode=200;
-            response.end('Ini adalah homepage');
+            response.end(JSON.stringify({
+                message: "Ini adalah homepage",
+            }));
         } else {
             response.statusCode=400;
-            response.end(`Halaman tidak dapat diakses dengan ${method} request`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }))
         }
     }
     else if (url === "/about") {
         if(method === "GET") {
             response.statusCode=200;
-            response.end(`Halo! ini adalah halaman about`);
+            response.end(JSON.stringify({
+                message: `Halo! ini adalah halaman about`,
+            }))
         } else if(method === "POST") {
             response.statusCode=200;
             let body = [];
@@ -28,20 +34,26 @@ const requestListener = (request, response) => {
             request.on("end", () => {
                body = Buffer.concat(body).toString();
                const {name} = JSON.parse(body);
-               response.end(`Halo, ${name}! ini adalah halaman about`);
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! ini adalah halaman about` 
+                }))
             });
           
         } else {
             response.statusCode=400;
-            response.end(`Halaman tidak dapat diakses dengan ${method} request`);
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }))
         }
     }
     else {
         response.statusCode=404;
-        response.end("Halaman tidak ditemukan!");
+        response.end(JSON.stringify({
+            message: "Halaman tidak ditemukan!",
+        }))
     }
 };
- 
+
 const server = http.createServer(requestListener);
  
 const PORT = 5000;
